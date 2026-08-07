3ea4e9af8ac885be3d1bb3c8febbe3e5dd891402
teeshirtboi <rahul200476@gmail.com>
2026-08-06 21:34:11 -0400

    Replace Flask development server with Gunicorn

--- webserver/configfiles/docker-entrypoint.sh
+++ webserver/configfiles/docker-entrypoint.sh
@@ -4,4 +4,4 @@
 nginx
 
 # Start the Flask application as the non-root user
-su -s /bin/bash appuser -c "python /app/app.py"
+su -s /bin/bash appuser -c "cd /app && exec gunicorn --bind 0.0.0.0:5000 --workers 2 app:app"
